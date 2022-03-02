92afeda46637704dc0d751a6b45930eff5756097
Mohamed Ayoub Idrissi <ayoub@hiddenclouders.com>
2022-03-02 14:56:54 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const cron = require("node-cron");
+let shell = require("shelljs");
+let cmp=0
+cron.schedule("* * * * * *", function(){
+    cmp++;
+    console.log("Scheduler running ...", cmp);
+    if(shell.exec("dir").code !== 0){
+        console.log("Something went wrong")
+    }
+})
